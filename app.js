4176770232fb1f384e85d82c54cb18c81f218aa2
olegsolomin <solomin.o.n@gmail.com>
2021-04-06 18:04:20 +0300

    not bad result

--- app.js
+++ app.js
@@ -11,21 +11,21 @@
         $scope.MealsValue = ""
         $scope.checkMeals = function () {
             var MealsQuantity =  calculateMeals($scope.Meals);
-            $scope.MealsValue = MealsQuantity;
-            // if (MealsQuantity === 0) {
-            //     $scope.MealsValue = 'Please enter data first';
-            // }
-            // if (MealsQuantity <= 3) {
-            //     $scope.MealsValue = 'Enjoy!';
-            // } else {
-            //     $scope.MealsValue = 'Too much!';
-            // };
-            };
-            
+            //$scope.MealsValue = MealsQuantity;
+            if ($scope.Meals === "") {
+                $scope.MealsValue = 'Please enter data first';
+            }
+            else {
+                if (MealsQuantity <= 3) {
+                $scope.MealsValue = 'Enjoy!';
+                } else {
+                $scope.MealsValue = 'Too much!';
+                }
+            }
+            }
             function calculateMeals(string) {
-                var MealsArray = "";
-                MealsArray = string.split(',');
-                console.log(MealsArray.length);
+                var MealsArray = [];
+                MealsArray = string.trim().split(',');
                 var ArrayMealsCount = 0;
                 ArrayMealsCount = MealsArray.length;
                 return ArrayMealsCount;
