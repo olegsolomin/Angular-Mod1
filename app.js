de0363ea8c4955237c1067e830b838d060d960df
olegsolomin <solomin.o.n@gmail.com>
2021-04-06 16:48:06 +0300

    more or less

--- app.js
+++ app.js
@@ -3,8 +3,35 @@
 
     angular.module('LunchCheck', [])
 
-        .controller('LunchCheckController', function () {
+    .controller('LunchCheckController', LunchCheckController);
+        
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController ($scope){
+        $scope.Meals = "";
+        $scope.MealsValue = ""
+        $scope.checkMeals = function () {
+            var MealsQuantity =  calculateMeals($scope.Meals);
+            $scope.MealsValue = MealsQuantity;
+            // if (MealsQuantity === 0) {
+            //     $scope.MealsValue = 'Please enter data first';
+            // }
+            // if (MealsQuantity <= 3) {
+            //     $scope.MealsValue = 'Enjoy!';
+            // } else {
+            //     $scope.MealsValue = 'Too much!';
+            // };
+            };
+            
+            function calculateMeals(string) {
+                var MealsArray = "";
+                MealsArray = string.split(',');
+                console.log(MealsArray.length);
+                var ArrayMealsCount = 0;
+                ArrayMealsCount = MealsArray.length;
+                return ArrayMealsCount;
+            }
 
-        });
+        
+    }
 
 })();
